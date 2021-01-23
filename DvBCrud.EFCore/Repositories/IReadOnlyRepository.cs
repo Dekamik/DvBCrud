b48f7c644b118ce44d0d46e23f0bc4a61cd19972
Dennis von Bargen <dennis@svrvive.com>
2021-01-23 15:48:26 +0100

    Documentation for IReadOnlyRepository and IRepository

--- DvBCrud.EFCore/Repositories/IReadOnlyRepository.cs
+++ DvBCrud.EFCore/Repositories/IReadOnlyRepository.cs
@@ -4,12 +4,31 @@ using System.Linq;
 
 namespace DvBCrud.EFCore.Repositories
 {
+    /// <summary>
+    /// A read-only repository for querying untracked <typeparamref name="TEntity"/> instances in database
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TId"><typeparamref name="TEntity"/> key type</typeparam>
     public interface IReadOnlyRepository<TEntity, TId> where TEntity : BaseEntity<TId>
     {
+        /// <summary>
+        /// Gets all entities
+        /// </summary>
+        /// <returns>An <see cref="IQueryable"/> containing all <typeparamref name="TEntity"/> instances</returns>
         IQueryable<TEntity> GetAll();
 
+        /// <summary>
+        /// Finds a single <typeparamref name="TEntity"/> whose Id matches <paramref name="id"/>
+        /// </summary>
+        /// <param name="id">Key value to query</param>
+        /// <returns>The matching <typeparamref name="TEntity"/> or null if it doesn't exist</returns>
         TEntity Get(TId id);
 
+        /// <summary>
+        /// Finds multiple <typeparamref name="TEntity"/> instances whose Ids are contained within <paramref name="ids"/>
+        /// </summary>
+        /// <param name="ids">Key values to query</param>
+        /// <returns>an <see cref="IQueryable"/> containing all, some or no matching <typeparamref name="TEntity"/> instances</returns>
         IQueryable<TEntity> GetRange(IEnumerable<TId> ids);
     }
 }
--- DvBCrud.EFCore/Repositories/IRepository.cs
+++ DvBCrud.EFCore/Repositories/IRepository.cs
@@ -4,21 +4,56 @@ using System.Threading.Tasks;
 
 namespace DvBCrud.EFCore.Repositories
 {
+    /// <summary>
+    /// A repository for Creating, Reading, Updating and Deleting <typeparamref name="TEntity"/> instances
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TId"><typeparamref name="TEntity"/> key type</typeparam>
     public interface IRepository<TEntity, TId> : IReadOnlyRepository<TEntity, TId> 
         where TEntity : BaseEntity<TId>
     {
+        /// <summary>
+        /// Creates the specified <paramref name="entity"/> and tracks it
+        /// </summary>
+        /// <param name="entity">Entity to create</param>
         void Create(TEntity entity);
 
+        /// <summary>
+        /// Creates all specified <paramref name="entities"/> and tracks them
+        /// </summary>
+        /// <param name="entities">Entities to create</param>
         void CreateRange(IEnumerable<TEntity> entities);
 
+        /// <summary>
+        /// Finds the tracked <typeparamref name="TEntity"/> by Id and copies values from the <paramref name="entity"/> to its corresponding tracked <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity">New entity data</param>
+        /// <param name="createIfNotExists">If true, creates <paramref name="entity"/> if it isn't found. <see cref="SaveChanges"/> should be called after invocation if used.</param>
         void Update(TEntity entity, bool createIfNotExists = false);
 
+        /// <summary>
+        /// Finds tracked <typeparamref name="TEntity"/> instances by Id and copies values from each <paramref name="entities"/> to their corresponding tracked <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entities">New entity data</param>
+        /// <param name="createIfNotExists">If true, creates all <paramref name="entities"/> that aren't found. <see cref="SaveChanges"/> should be called after invocation if used.</param>
         void UpdateRange(IEnumerable<TEntity> entities, bool createIfNotExists = false);
 
+        /// <summary>
+        /// Finds and removes the <typeparamref name="TEntity"/> whose Id matches <paramref name="id"/>
+        /// </summary>
+        /// <param name="id">Id of <typeparamref name="TEntity"/> to remove</param>
         void Delete(TId id);
 
+        /// <summary>
+        /// Finds and removes all <typeparamref name="TEntity"/> instances whose Ids matches those found in <paramref name="ids"/>
+        /// </summary>
+        /// <param name="ids">Ids of all <typeparamref name="TEntity"/> instances to remove</param>
         void DeleteRange(IEnumerable<TId> ids);
 
+        /// <summary>
+        /// Persists modifications to DbContext
+        /// </summary>
+        /// <returns>An async Task</returns>
         Task SaveChanges();
     }
 }
