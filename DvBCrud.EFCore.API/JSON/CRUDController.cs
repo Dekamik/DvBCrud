bd78a4770f5f09f2ad1f48404361cb7f06ea167d
Dennis von Bargen <dennis@svrvive.com>
2021-01-26 20:24:14 +0100

    GUID for logging

--- DvBCrud.EFCore.API/JSON/CRUDController.cs
+++ DvBCrud.EFCore.API/JSON/CRUDController.cs
@@ -3,6 +3,7 @@ using DvBCrud.EFCore.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,66 +23,78 @@ namespace DvBCrud.EFCore.API.JSON
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] TEntity entity)
         {
-            logger.LogTrace($"{nameof(Create)} {nameof(TEntity)}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Create)} {nameof(TEntity)} ({guid})");
 
             repository.Create(entity);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Create)} OK ({guid})");
             return Ok();
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody]IEnumerable<TEntity> entities)
         {
-            logger.LogDebug($"{nameof(Create)} {entities.Count()} {nameof(TEntity)}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Create)} {entities.Count()} {nameof(TEntity)} ({guid})");
 
             repository.CreateRange(entities);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Create)} OK ({guid})");
             return Ok();
         }
 
         [HttpPut]
         public async Task<IActionResult> Update([FromBody] TEntity entity, [FromQuery] bool createIfNotExists = false)
         {
-            logger.LogDebug($"{nameof(Update)} {nameof(TEntity)}.Id = {entity}{(createIfNotExists ? ", createIfNotExists = true" : "")}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Update)} {nameof(TEntity)}.Id = {entity}{(createIfNotExists ? ", createIfNotExists = true" : "")} ({guid})");
 
             await repository.Update(entity, createIfNotExists);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Update)} OK ({guid})");
             return Ok();
         }
 
         [HttpPut]
         public async Task<IActionResult> Update([FromBody]IEnumerable<TEntity> entities, [FromQuery]bool createIfNotExists = false)
         {
-            logger.LogDebug($"{nameof(Update)} {entities.Count()} {nameof(TEntity)}.Id = {string.Join(", ", entities)}{(createIfNotExists ? ", createIfNotExists = true" : "")}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Update)} {entities.Count()} {nameof(TEntity)}.Id = {string.Join(", ", entities)}{(createIfNotExists ? ", createIfNotExists = true" : "")} ({guid})");
 
             repository.UpdateRange(entities, createIfNotExists);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Update)} OK ({guid})");
             return Ok();
         }
 
         [HttpDelete, Route("{id}")]
         public async Task<IActionResult> Delete([FromQuery]TId id)
         {
-            logger.LogDebug($"{nameof(Delete)} {nameof(TEntity)}.Id = {id}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Delete)} {nameof(TEntity)}.Id = {id} ({guid})");
 
             repository.Delete(id);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Delete)} OK ({guid})");
             return Ok();
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete([FromBody]IEnumerable<TId> id)
         {
-            logger.LogDebug($"{nameof(Delete)} {nameof(TEntity)}.Id = {string.Join(", ", id)}");
+            var guid = Guid.NewGuid();
+            logger.LogDebug($"{nameof(Delete)} {nameof(TEntity)}.Id = {string.Join(", ", id)} ({guid})");
 
             repository.DeleteRange(id);
             await repository.SaveChanges();
 
+            logger.LogDebug($"{nameof(Delete)} OK ({guid})");
             return Ok();
         }
     }
