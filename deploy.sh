55ac39da9bf58d7f2c4b8fd70ac05324d02e75d9
Dennis von Bargen <dennis@vonbargen.se>
2021-01-28 17:58:29 +0100

    Fixed pack command

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ IFS=$'\n\t'
 key=$1
 src=$2
 
-dotnet nuget pack DvBCrud.EFCore/DvBCrud.EFCore.csproj
-dotnet nuget pack DvBCrud.EFCore.API/DvBCrud.EFCore.API.csproj
+dotnet pack -c Release DvBCrud.EFCore/DvBCrud.EFCore.csproj
+dotnet pack -c Release DvBCrud.EFCore.API/DvBCrud.EFCore.API.csproj
 
-dotnet nuget push DvBCrud.EFCore/bin/Debug/DvBCrud.EFCore.*.nupkg --api-key $key --source $src
-dotnet nuget push DvBCrud.EFCore.API/bin/Debug/DvBCrud.EFCore.API.*.nupkg --api-key $key --source $src
+dotnet nuget push DvBCrud.EFCore/bin/Release/DvBCrud.EFCore.*.nupkg --api-key $key --source $src
+dotnet nuget push DvBCrud.EFCore.API/bin/Release/DvBCrud.EFCore.API.*.nupkg --api-key $key --source $src
