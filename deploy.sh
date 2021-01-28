c7e680a19ea8032f685afa7fc20a797d75710262
Dennis von Bargen <dennis@vonbargen.se>
2021-01-28 18:29:24 +0100

    Now deploys on tags

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-key=$1
-src=$2
+tag=$1
+key=$2
+src=$3
 
-dotnet pack -c Release DvBCrud.EFCore/DvBCrud.EFCore.csproj
-dotnet pack -c Release DvBCrud.EFCore.API/DvBCrud.EFCore.API.csproj
+dotnet pack -c Release -p:PackageVersion=$tag DvBCrud.EFCore/DvBCrud.EFCore.csproj
+dotnet pack -c Release -p:PackageVersion=$tag DvBCrud.EFCore.API/DvBCrud.EFCore.API.csproj
 
 dotnet nuget push DvBCrud.EFCore/bin/Release/DvBCrud.EFCore.*.nupkg --api-key $key --source $src
 dotnet nuget push DvBCrud.EFCore.API/bin/Release/DvBCrud.EFCore.API.*.nupkg --api-key $key --source $src
