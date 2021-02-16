f105accd1d59824574bf96e0ba11d76aa3983a71
Dennis von Bargen <dennis@vonbargen.se>
2021-02-16 15:22:07 +0100

    Removed ReadOnlyRepository

--- DvBCrud.EFCore.Mocks/Repositories/IAnyReadOnlyRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DvBCrud.EFCore.Mocks.Entities;
-using DvBCrud.EFCore.Repositories;
-
-namespace DvBCrud.EFCore.Mocks.Repositories
-{
-    public interface IAnyReadOnlyRepository : IReadOnlyRepository<AnyEntity, int>
-    {
-    }
-}
